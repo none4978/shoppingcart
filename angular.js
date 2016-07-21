a02eaa8761e03a7de743831f71b85d0cdd9ae6a5
eric yang <eric yang@ADMINIB-U74NQLB>
2016-07-21 15:15:14 +0800

    with button style

--- angular.js
+++ angular.js
@@ -8,15 +8,15 @@ var item = [
 ];
 var shopCar=[];
 var app=angular.module("myApp",[]);
-app.controller("showItem",function($scope){
+app.controller("Item",function($scope){
     $scope.total=0;
     $scope.items=item;
     $scope.shopCar=shopCar;
      
     $scope.DelFromShopCarList=function(){
-        $scope.total-=this.value.price;
+        $scope.total-=this.x.price;
         for(var i=0;i<$scope.shopCar.length;i++){
-            if($scope.shopCar[i].id==this.value.id){
+            if($scope.shopCar[i].id==this.x.id){
                 $scope.shopCar[i].amount-=1;
                 if($scope.shopCar[i].amount===0){
                     $scope.shopCar.splice(i,1)
@@ -27,17 +27,16 @@ app.controller("showItem",function($scope){
      
      
     $scope.addToShopCarList=function(){
- 
         var item={
-                name:this.value.name,
-                price:this.value.price,
-                id:this.value.id,
+                name:this.x.name,
+                price:this.x.price,
+                id:this.x.id,
                 amount:1,
         };
         var len=shopCar.length;
         var inArr=true;
         for(var i=0;i<len;i++){
-            if(shopCar[i].id===this.value.id){
+            if(shopCar[i].id===this.x.id){
                 inArr=false;
                 shopCar[i].amount+=1;
                 break;
@@ -50,4 +49,30 @@ app.controller("showItem",function($scope){
          
         $scope.total+=shopCar[i].price;
     }
-})
+    
+    $scope.opentitle=function(){
+    	$scope.title = '商品列表';	
+    }
+});
+
+
+app.directive('expander', function() {
+    return {
+        restrict : 'EA',
+        replace : true,
+        transclude : true,
+        scope : {
+            title : '=expanderTitle'
+        },
+        template : '<div>'
+                 + '<div class="title" ng-click="toggle()">{{title}}</div>'
+                 + '<div class="body" ng-show="showMe" ng-transclude></div>'
+                 + '</div>',
+        link : function(scope, element, attrs) {
+            scope.showMe = false;
+            scope.toggle = function toggle() {
+                scope.showMe = !scope.showMe;
+            }
+        }
+    }
+});
